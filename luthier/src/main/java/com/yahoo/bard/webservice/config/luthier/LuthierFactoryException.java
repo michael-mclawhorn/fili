6d0f58f7b59459aaab1e6d92c804c975f1bcabfc
Gabriel Luo <luogm@umich.edu>
2019-06-18 15:34:36 -0500

    fixed a merge conflict that I did not see

--- luthier/src/main/java/com/yahoo/bard/webservice/config/luthier/LuthierFactoryException.java
+++ luthier/src/main/java/com/yahoo/bard/webservice/config/luthier/LuthierFactoryException.java
@@ -12,11 +12,7 @@ public class LuthierFactoryException extends RuntimeException {
      *
      * @param message  Error message text
      */
-<<<<<<< HEAD
-    LuthierFactoryException(String message) {
-=======
     public LuthierFactoryException(String message) {
->>>>>>> 11deba8e7094d18dca7cd7237f20009bf9164fc5
         super(message);
     }
 
@@ -26,11 +22,7 @@ public class LuthierFactoryException extends RuntimeException {
      * @param message Error message text
      * @param cause  throwable triggering this exception.
      */
-<<<<<<< HEAD
-    LuthierFactoryException(String message, Throwable cause) {
-=======
     public LuthierFactoryException(String message, Throwable cause) {
->>>>>>> 11deba8e7094d18dca7cd7237f20009bf9164fc5
         super(message, cause);
     }
 }
